dbc5d8f66849a7fe1025f71b1e231bb28383c8fa
Jan Suchomel <jansuchomel@gmx.com>
2016-04-02 11:20:03 +0200

    Prepare library and playlist.

---
+++ src/reducers/LibraryReducer.js
@@ -0,0 +1,10 @@
+let defaultState = {
+  tracks: []
+};
+
+export default function(state = defaultState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -2,5 +2,12 @@ import { combineReducers } from 'redux';
 
 import PlayerReducer from './PlayerReducer';
 import TrackReducer from './TrackReducer';
+import PlaylistReducer from './PlaylistReducer';
+import LibraryReducer from './LibraryReducer';
 
-export default combineReducers({ player: PlayerReducer, track: TrackReducer });
+export default combineReducers({
+  player: PlayerReducer,
+  track: TrackReducer,
+  playlist: PlaylistReducer,
+  library: LibraryReducer
+});
