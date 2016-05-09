bf3b84dbb1abc5d5e37fcd15578f00fddf277245
Jan Suchomel <jansuchomel@gmx.com>
2016-05-09 23:41:35 +0200

    Sort tracks by album order in the library. (+hiding in the tree WIP)

--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -11,7 +11,8 @@ export default function(state = defaultState, action) {
                         mutatedState.set(library.name, Map({
                             name: library.name,
                             uri: library.uri,
-                            artists: Map({})
+                            artists: Map({}),
+                            visible: true
                         }));
                     }
                 }
@@ -21,24 +22,29 @@ export default function(state = defaultState, action) {
             return state.setIn([action.library, "artists"], library.withMutations(mutatedLibrary => {
                 for (let artist of action.artists) {
                     if (artist.name.trim() == "") continue; // empty google music artist workaround
-                    mutatedLibrary.set(artist.name, Map({uri: artist.uri, albums: Map({}), name: artist.name}));
+                    mutatedLibrary.set(artist.name, Map({uri: artist.uri, albums: Map({}), name: artist.name, visible: true}));
                 }
             }).sortBy(artist => artist.get("name"))
         );
         case "ARTIST_EXPANDED":
             return state.withMutations(mutatedState => {
                 for (let album of action.albums) {
-                    mutatedState.setIn([action.library, "artists", action.artist, "albums", album.name], Map({uri: album.uri, tracks: Map({}), name:album.name}));
+                    mutatedState.setIn([action.library, "artists", action.artist, "albums", album.name], Map({uri: album.uri, tracks: Map({}), name:album.name, visible: true}));
                 }});
         case "ALBUM_EXPANDED":
             return state.withMutations(mutatedState => {
                 for (let track of action.tracks) {
                     mutatedState.setIn([action.library, "artists", action.artist, "albums", action.album, "tracks", track.name],
-                    Map(track));
+                    Map(track).set("visible", true).set("track_no", -1));
                 }});
         case "TRACK_EXPANDED":
-            return state.setIn([action.library, "artists", action.artist, "albums", action.album, "tracks", action.track],
-                Map(action.details));
+            return state.setIn(
+                [action.library, "artists", action.artist, "albums", action.album, "tracks"],
+                state.getIn(
+                    [action.library, "artists", action.artist, "albums", action.album, "tracks"])
+                .set(
+                    action.track, Map(action.details).set("visible", true))
+                .sortBy(track => track.get("track_no")));
         default:
             return state;
     }
